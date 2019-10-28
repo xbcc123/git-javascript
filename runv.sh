abbd5a35963daf4368769e036a1ed50b03cce974
yankangjie <287207951@qq.com>
2019-10-28 22:57:18 +0800

    222

--- runv.sh
+++ runv.sh
@@ -1,5 +1,5 @@
 mkdir version
 cd version
-# git init
-# git remote add origin https://gitlab.com/287207951/version.git
-# git pull origin master
+git init
+git remote add origin https://gitlab.com/287207951/version.git
+git pull origin master
